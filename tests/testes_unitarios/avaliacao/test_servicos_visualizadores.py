fad95150fad55124952fff57758774349b948875
othonbreener <othonbrener123@gmail.com>
2022-09-19 09:42:15 -0300

    tests: começando a escrever testes para os servicos da avaliacao

---
+++ tests/testes_unitarios/avaliacao/test_servicos_visualizadores.py
@@ -0,0 +1,26 @@
+from garcom.contextos_de_negocio.estrutura_de_provas.dominio.agregados.avaliacao import (
+    Avaliacao
+)
+from garcom.contextos_de_negocio.estrutura_de_provas.dominio.agregados.exercicio import Exercicio
+from garcom.contextos_de_negocio.estrutura_de_provas.servicos.visualizadores.avaliacao import (
+    consultar_avaliacoes
+)
+
+
+def test_consultar_todos_avaliacoes(mock_uow):
+    
+    # Primeiro casdastrar um exercicio
+    with mock_uow:
+        mock_uow.repo_consulta._adicionar(
+            Exercicio(
+                materia='Matematica',
+                assunto='geometria',
+                dificuldade='facil',
+                enunciado='Quanto é dois mais dois?',
+            )
+        )
+    
+    # Cadastrar uma avaliacao
+    
+    # Testar a funcao de consulta
+    ...
