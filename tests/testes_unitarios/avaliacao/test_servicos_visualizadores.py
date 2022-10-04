ba7a66f8505c59f856d99a3698b48c4bb23b0c90
othonbreener <othonbrener123@gmail.com>
2022-10-04 14:08:42 -0300

    tests(tests/testes_unitarios/avaliacao): adicionando testes no servico de visualizacao da avaliacao

--- tests/testes_unitarios/avaliacao/test_servicos_visualizadores.py
+++ tests/testes_unitarios/avaliacao/test_servicos_visualizadores.py
@@ -5,22 +5,53 @@ from garcom.contextos_de_negocio.estrutura_de_provas.dominio.agregados.exercicio
 from garcom.contextos_de_negocio.estrutura_de_provas.servicos.visualizadores.avaliacao import (
     consultar_avaliacoes
 )
+from garcom.adaptadores.tipos_nao_primitivos.exercicio import (
+    Dificuldade, Materia, AssuntosMatematica
+)
+from garcom.contextos_de_negocio.estrutura_de_provas.dominio.comandos.avaliacao import (
+    CriarAvaliacao
+)
+from garcom.contextos_de_negocio.estrutura_de_provas.dominio.comandos.exercicio import (
+    CriarExercicio
+)
+from garcom.adaptadores.tipos_nao_primitivos.avaliacao import TipoDeAvaliacao
 
 
 def test_consultar_todos_avaliacoes(mock_uow):
     
-    # Primeiro casdastrar um exercicio
+    # criando exercicios
+    comando = CriarExercicio(
+        materia=Materia.matematica,
+        assunto='Operações Básicas',
+        dificuldade=Dificuldade.facil,
+        enunciado='Quanto é dois mais dois?',
+        resposta='4'
+    )
+
+    exercicio = Exercicio.criar_novo(comando)
     with mock_uow:
-        mock_uow.repo_consulta._adicionar(
-            Exercicio(
-                materia='Matematica',
-                assunto='geometria',
-                dificuldade='facil',
-                enunciado='Quanto é dois mais dois?',
-            )
-        )
-    
-    # Cadastrar uma avaliacao
-    
+        mock_uow.repo_consulta.adicionar(exercicio)
+
+    # adicionando avaliacao
+    comando = CriarAvaliacao(
+        titulo = 'Avaliação de Matemática',
+        responsavel= 'Othon',
+        tipo_de_avaliacao=TipoDeAvaliacao.avaliacao_ensino_medio,
+        exercicios={exercicio.id}
+    )
+
+    avaliacao = Avaliacao.criar_avaliacao(comando, {exercicio})
+    with mock_uow:
+        mock_uow.repo_consulta.adicionar(avaliacao)
+
+    #verificando se o exercicio e a avaliacao estão salvas
+    with mock_uow:
+        assert mock_uow.repo_consulta.consultar_todos_por_agregado(Exercicio)
+        assert mock_uow.repo_consulta.consultar_todos_por_agregado(Avaliacao)
+
     # Testar a funcao de consulta
-    ...
+    avaliacoes = consultar_avaliacoes(mock_uow)
+    
+    for avaliacao in avaliacoes:
+        if issubclass(avaliacao.__class__, Avaliacao):
+            assert avaliacao.id == avaliacao.id
