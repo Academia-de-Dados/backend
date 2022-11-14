a855906f0d4dfb752d1faf1209b8771e6804a8fd
othonbreener <othonbrener123@gmail.com>
2022-11-13 22:49:04 -0300

    Adicionando serviço de manipulação de eventos e comandos.

--- garcom/barramento.py
+++ garcom/barramento.py
@@ -7,3 +7,13 @@ class Comando(ABC):
 
 class Evento(ABC):
     pass
+
+
+class OnibusDeMenssagem:
+    def __init__(self, unidade_de_trabalho, manipuladores_de_eventos, manipuladores_de_comandos):
+        self.unidade_de_trabalho = unidade_de_trabalho
+        self.manipuladores_de_eventos = manipuladores_de_eventos
+        self.manipuladores_de_comandos = manipuladores_de_comandos
+
+    def manipulador(self):
+        ...
