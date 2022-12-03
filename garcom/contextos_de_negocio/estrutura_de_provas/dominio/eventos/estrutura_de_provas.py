38ab99fff8e12b276f69cdef05205bcdca7b52d0
othonbreener <othonbrener123@gmail.com>
2022-12-03 19:55:07 -0300

    fix(garcom/contextos_de_negocio/estrutura_de_provas/dominio/eventos/estrutura_de_provas.py): corrige erro de digitação no atributo do decorador e adiciona atributo ao evento de email

--- garcom/contextos_de_negocio/estrutura_de_provas/dominio/eventos/estrutura_de_provas.py
+++ garcom/contextos_de_negocio/estrutura_de_provas/dominio/eventos/estrutura_de_provas.py
@@ -2,14 +2,16 @@ from dataclasses import dataclass
 from garcom.barramento import Evento
 from garcom.adaptadores.tipos_nao_primitivos.tipos import ExercicioId, AvaliacaoId
 
+@dataclass(frozen=True)
 class EnviarEmail(Evento):
-    ...
+    mensagem: str
 
-@dataclass(fronzen=True)
+
+@dataclass(frozen=True)
 class ExercicioCriado(Evento):
     exercicio_id: ExercicioId
 
 
-@dataclass(fronzen=True)
+@dataclass(frozen=True)
 class AvaliacaoCriada(Evento):
     avaliacao_id: AvaliacaoId
