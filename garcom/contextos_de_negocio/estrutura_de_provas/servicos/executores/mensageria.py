67b024d1c89173d7cd80c09791946c68500bc8d5
othonbreener <othonbrener123@gmail.com>
2022-12-03 20:03:17 -0300

    feat(garcom/contextos_de_negocio/estrutura_de_provas/servicos/executores/mensageria.py): adiciona metodo não implementado para enviar emails

---
+++ garcom/contextos_de_negocio/estrutura_de_provas/servicos/executores/mensageria.py
@@ -0,0 +1,15 @@
+from ...dominio.eventos.estrutura_de_provas import EnviarEmail,
+from garcom.camada_de_servicos.unidade_de_trabalho.udt import (
+    UnidadeDeTrabalhoAbstrata,
+)
+
+
+def enviar_email(
+    evento: EnviarEmail, unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
+):
+    """
+    Implementar envio de emails
+    """
+    ...
+    
+    print(evento)
