fde7bdd133a5d31597119d5b79ddec020d9c9c5e
othonbreener <othonbrener123@gmail.com>
2022-12-03 19:51:19 -0300

    refat(garcom/contextos_de_negocio/agregado.py): altera interface de agregado para dataclass e define eventos como uma lista vazia por padrao

--- garcom/contextos_de_negocio/agregado.py
+++ garcom/contextos_de_negocio/agregado.py
@@ -1,14 +1,13 @@
 from abc import ABC
 
 from ..barramento import Evento
+from dataclasses import dataclass, field
 
 
+@dataclass
 class Agregado(ABC):
 
-    eventos: list[Evento]
-    
-    def __init__(self):
-        self.eventos = []
+    eventos: list[Evento] = field(default_factory=list)
 
     def adicionar_evento(self, evento: Evento):
         assert issubclass(type(evento), Evento)
