d4fb8183562c43a7261e1881cd7b871f885f1216
othonbreener <othonbrener123@gmail.com>
2022-12-27 10:15:59 -0300

    refat(garcom/contextos_de_negocio/estrutura_de_provas/repositorio/repo_dominio): altera o repositorio de avaliacao e exercicio para herdar do repositorio abstrato de dominio padrão

--- garcom/contextos_de_negocio/estrutura_de_provas/repositorio/repo_dominio/dominio_avaliacao.py
+++ garcom/contextos_de_negocio/estrutura_de_provas/repositorio/repo_dominio/dominio_avaliacao.py
@@ -5,43 +5,8 @@ from garcom.adaptadores.orm.repositorio import RepositorioAbstratoDominio
 from ...dominio.agregados.avaliacao import Avaliacao
 from garcom.adaptadores.tipos_nao_primitivos.tipos import AvaliacaoId
 
-class AvaliacaoAbstratoDominio(RepositorioAbstratoDominio):
-    """
-    Repositorio abstrato de dominio.
 
-    Implementa os métodos que alteram o banco de dados.
-    """
-
-    def adicionar(self, agregado):
-        self._adicionar(agregado)
-        self.agregados.add(agregado)
-
-    def remover(self, agregado):
-        self._remover(agregado)
-        self.agregados.add(agregado)
-
-    def buscar_por_id(self, avaliacao_id: AvaliacaoId):
-        agregado = self._buscar_por_id(avaliacao_id)
-        if agregado:
-            self.agregados.add(agregado)
-        return agregado
-
-    @abstractmethod
-    def _adicionar(self) -> None:
-        """Adiciona uma nova avaliacao no banco."""
-        raise NotImplementedError
-
-    @abstractmethod
-    def _remover(self) -> None:
-        """Remove uma avaliacao do banco."""
-        raise NotImplementedError
-
-    @abstractmethod
-    def _buscar_por_id(self, avaliacao_id: AvaliacaoId) -> None:
-        raise NotImplementedError
-
-
-class AvaliacaoRepoDominio(AvaliacaoAbstratoDominio):
+class AvaliacaoRepoDominio(RepositorioAbstratoDominio):
     """
     Repositorio concreto de dominio.
 
--- garcom/contextos_de_negocio/estrutura_de_provas/repositorio/repo_dominio/dominio_exercicios.py
+++ garcom/contextos_de_negocio/estrutura_de_provas/repositorio/repo_dominio/dominio_exercicios.py
@@ -6,49 +6,7 @@ from ...dominio.agregados.exercicio import Exercicio
 from garcom.adaptadores.tipos_nao_primitivos.tipos import ExercicioId
 
 
-class ExercicioAbstratoDominio(RepositorioAbstratoDominio):
-    """
-    Repositorio abstrato de dominio.
-
-    Classe utilizada para implementar os métodos
-    que alteram o banco de dados.
-    
-    O atributo 'agregados' é utilizado para adicionar as instancias
-    dos agregados iniciados, para poder coletar os eventos emitidos
-    por eles. Apenas repositorio de dominio emitir eventos, não usar
-    no de consulta.
-    """
-
-    def adicionar(self, agregado):
-        self._adicionar(agregado)
-        self.agregados.add(agregado)
-
-    def remover(self, agregado):
-        self._remover(agregado)
-        self.agregados.add(agregado)
-
-    def buscar_por_id(self, exercicio_id: ExercicioId):
-        agregado = self._buscar_por_id(exercicio_id)
-        if agregado:
-            self.agregados.add(agregado)
-        return agregado
-
-    @abstractmethod
-    def _adicionar(self, exercicio: Exercicio) -> None:
-        """Adiciona um novo exercicio ao banco."""
-        raise NotImplementedError
-
-    @abstractmethod
-    def _remover(self) -> None:
-        """Remove um exercicio do banco."""
-        raise NotImplementedError
-
-    @abstractmethod
-    def _buscar_por_id(self, exercicio_id: ExercicioId) -> None:
-        raise NotImplementedError
-
-
-class ExercicioRepoDominio(ExercicioAbstratoDominio):
+class ExercicioRepoDominio(RepositorioAbstratoDominio):
     """
     Repositorio concreto de dominio.
 
