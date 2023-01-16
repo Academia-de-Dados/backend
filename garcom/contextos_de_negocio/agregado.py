407c80371981e1235708e14efd61d9479db8919d
othonbreener <othonbrener123@gmail.com>
2023-01-15 22:23:18 -0300

    refat(garcom/contextos_de_negocio/agregado.py): removendo field do atributo eventos

--- garcom/contextos_de_negocio/agregado.py
+++ garcom/contextos_de_negocio/agregado.py
@@ -2,12 +2,15 @@ from abc import ABC
 
 from ..barramento import Evento
 from dataclasses import dataclass, field
+from typing import ClassVar, List
 
 
-@dataclass
 class Agregado(ABC):
-
-    eventos: list[Evento] = field(default_factory=list)
+    """
+    O atributo eventos deve ser um tipo classvar para que as classes
+    que herdam de Agregado não dê erro no
+    """
+    eventos: List[Evento]
 
     def adicionar_evento(self, evento: Evento):
         assert issubclass(type(evento), Evento)
