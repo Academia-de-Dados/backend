0a3e6737b0de9c7a8f4ffd9280e4cc01864c5ccf
othonbreener <othonbrener123@gmail.com>
2023-01-21 20:17:06 -0300

    feat(garcom/aplicacao/main.py): adiciona mapper do modelo usuario ao on_startup da api

--- garcom/aplicacao/main.py
+++ garcom/aplicacao/main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from ..contextos_de_negocio.estrutura_de_provas.pontos_de_entrada.api import (
     router_estrutura_de_provas,
 )
-from ..contextos_de_negocio.estrutura_de_provas.repositorio.orm import orm
+from garcom.contextos_de_negocio.identidade_e_acesso.repositorio.orm.usuario import (
+    start_mappers_usuario
+)
+from ..contextos_de_negocio.estrutura_de_provas.repositorio.orm.orm import (
+    init_database, start_mappers
+)
 
 app = FastAPI()
 app.include_router(router_estrutura_de_provas)
@@ -17,5 +22,6 @@ def rota_hellow():
 @app.on_event('startup')
 def on_startup() -> None:
     """Inicializa o banco de dados."""
-    orm.init_database()
-    orm.start_mappers()
+    init_database()
+    start_mappers()
+    start_mappers_usuario()
