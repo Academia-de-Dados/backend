c8bf95a08842a6969b92f2adeced04f2fb25727c
othonbreener <othonbrener123@gmail.com>
2023-01-22 19:03:13 -0300

    feat(garcom/contextos_de_negocio): adiciona rotas inicias para usuario e classes intermediarias do fastapi para o usuario

--- garcom/contextos_de_negocio/identidade_e_acesso/pontos_de_entrada/api.py
+++ garcom/contextos_de_negocio/identidade_e_acesso/pontos_de_entrada/api.py
@@ -4,4 +4,25 @@ from garcom.contextos_de_negocio.identidade_e_acesso.dominio.agregados.usuarios 
     Usuario,
 )
 
-router_usuarios = APIRouter()
+
+router_usuarios = APIRouter(prefix='/usuarios', tags=['Identidade e Acesso'])
+
+
+@router_usuarios.get(response_model='')
+def consultar_usuarios():
+    ...
+
+
+@router_usuarios.get('/{id}')
+def consultar_usuario_por_id():
+    ...
+    
+
+@router_usuarios.post('/signup')
+def cadastrar_usuario():
+    ...
+
+
+@router_usuarios.post('/signin')
+def logar_usuario():
+    ...
---
+++ garcom/contextos_de_negocio/identidade_e_acesso/pontos_de_entrada/representacoes.py
@@ -0,0 +1,9 @@
+from garcom.representacoes import MyBaseModel
+
+
+class UsuarioConsulta(MyBaseModel):
+    ...
+
+
+class UsuarioDominio(MyBaseModel):
+    ...
