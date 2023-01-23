2a80bb0f73519ebc7b301cae2de171c3b44530de
othonbreener <othonbrener123@gmail.com>
2023-01-23 19:22:58 -0300

    feat(garcom/contextos_de_negocio/identidade_e_acesso/repositorio): adiciona repos de dominio e consulta para o usuario

---
+++ garcom/contextos_de_negocio/identidade_e_acesso/repositorio/repo_consulta/usuario.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+from sqlalchemy.future import select
+from garcom.adaptadores.orm.repositorio import RepositorioAbstrato
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.agregados.usuarios import (
+    Usuario,
+)
+from garcom.adaptadores.tipos_nao_primitivos.tipos import UsuarioId
+
+
+class UsuariosAbstratoConsulta(RepositorioAbstrato):
+    @abstractmethod
+    def consultar_todos(self) -> list[Usuario]:
+        """Retorna todos os exercicios do banco dados."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def consultar_por_id(self, id: UsuarioId) -> Usuario:
+        """Retorna o exericios corresponde ao id passado."""
+        raise NotImplementedError
+
+
+class UsuariosRepoConsulta(UsuariosAbstratoConsulta):
+    
+    def consultar_todos(self) -> list[Usuario]:
+        query = self.session.execute(select(Usuario))
+        return query.scalars().all()
+
+    def consultar_por_id(self, id: UsuarioId) -> Usuario:
+        return self.session.query(Usuario).filter_by(id=id).first()
---
+++ garcom/contextos_de_negocio/identidade_e_acesso/repositorio/repo_dominio/usuario.py
@@ -0,0 +1,18 @@
+from garcom.adaptadores.orm.repositorio import RepositorioAbstratoDominio
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.agregados.usuarios import (
+    Usuario,
+)
+from garcom.adaptadores.tipos_nao_primitivos.tipos import UsuarioId
+
+
+class UsuariosRepoDominio(RepositorioAbstratoDominio):
+    
+    def _adicionar(self, usuario: Usuario) -> None:
+        self.session.add(usuario)
+    
+    
+    def _remover(self) -> None:
+        pass
+    
+    def _buscar_por_id(self, usuario_id: UsuarioId) -> None:
+        pass
