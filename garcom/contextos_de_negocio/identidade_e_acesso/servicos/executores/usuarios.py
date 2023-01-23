f05825f7ca72c33d7f8bcd96c29f2e6f8331f58f
othonbreener <othonbrener123@gmail.com>
2023-01-23 19:24:32 -0300

    feat(garcom/contextos_de_negocio/identidade_e_acesso/servicos): adiciona servicos de visualizacao e escrita para o usuario

---
+++ garcom/contextos_de_negocio/identidade_e_acesso/servicos/executores/usuarios.py
@@ -0,0 +1,33 @@
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.agregados.usuarios import (
+    Usuario,
+)
+from garcom.adaptadores.tipos_nao_primitivos.tipos import UsuarioId
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.comandos.usuario import (
+    CriarUsuario,
+)
+from garcom.camada_de_servicos.unidade_de_trabalho.udt import (
+    UnidadeDeTrabalhoAbstrata,
+)
+from garcom.adaptadores.dominio import Dominio
+
+
+def cadastrar_usuario(
+    comando: CriarUsuario, 
+    unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
+) -> UsuarioId:
+    
+    usuario = Usuario.cadrastar_novo_usuario(comando)
+    usuario_id = usuario.id
+    
+    with unidade_de_trabalho(Dominio.usuarios) as uow:
+        try:
+            #usuario.adicionar_evento()
+            uow.repo_dominio.adicionar(usuario)
+            uow.commit()
+
+        except Exception as e:
+            uow.rollback()
+            raise e
+
+    return usuario_id
+    
---
+++ garcom/contextos_de_negocio/identidade_e_acesso/servicos/visualizadores/usuarios.py
@@ -0,0 +1,30 @@
+from garcom.adaptadores.dominio import Dominio
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.agregados.usuarios import (
+    Usuario,
+)
+from garcom.camada_de_servicos.unidade_de_trabalho.udt import (
+    UnidadeDeTrabalhoAbstrata,
+)
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.comandos.usuario import (
+    BuscarTodosUsuarios, BuscarUsuarioPorId
+)
+
+
+def consultar_usuarios(
+    comando: BuscarTodosUsuarios,
+    unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
+) -> list[Usuario]:
+    with unidade_de_trabalho(Dominio.usuarios) as uow:
+        usuarios = uow.repo_consulta.consultar_todos()
+    
+    return usuarios
+
+
+def consultar_usuario_por_id(
+    comando: BuscarUsuarioPorId,
+    unidade_de_trabalho: UnidadeDeTrabalhoAbstrata,
+) -> Usuario:
+    with unidade_de_trabalho(Dominio.usuarios) as uow:
+        usuario = uow.repo_consulta.consultar_por_id(comando.usuario_id)
+    
+    return usuario
