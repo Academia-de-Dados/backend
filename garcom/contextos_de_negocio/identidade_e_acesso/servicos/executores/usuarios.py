39f96bbdd297898d16e6465ee619c6340db0c16d
othonbreener <othonbrener123@gmail.com>
2023-01-26 20:47:12 -0300

    feat(garcom/contextos_de_negocio/identidade_e_acesso/servicos/executores/usuarios.py): adiciona logica para verificar se já existe usuario com email passado no sistema e adiciona classe Senha para validar senhas

--- garcom/contextos_de_negocio/identidade_e_acesso/servicos/executores/usuarios.py
+++ garcom/contextos_de_negocio/identidade_e_acesso/servicos/executores/usuarios.py
@@ -4,6 +4,7 @@ from garcom.contextos_de_negocio.identidade_e_acesso.dominio.agregados.usuarios 
 from garcom.adaptadores.tipos_nao_primitivos.tipos import UsuarioId
 from garcom.contextos_de_negocio.identidade_e_acesso.dominio.comandos.usuario import (
     CriarUsuario,
+    BuscarUsuarioPorEmail
 )
 from garcom.camada_de_servicos.unidade_de_trabalho.udt import (
     UnidadeDeTrabalhoAbstrata,
@@ -12,7 +13,13 @@ from garcom.adaptadores.dominio import Dominio
 from garcom.contextos_de_negocio.identidade_e_acesso.dominio.regras_de_negocio.encriptografia import (
     gerar_senha_encriptografada,
 )
-
+from garcom.adaptadores.tipos_nao_primitivos.usuario import Senha
+from garcom.contextos_de_negocio.identidade_e_acesso.excecoes import (
+    SenhasDiferentes, EmailJaCadastrado
+)
+from garcom.contextos_de_negocio.identidade_e_acesso.servicos.visualizadores.usuarios import (
+    consultar_usuario_por_email
+)
 # TODO: fazer query por email para verificar se já existe usuario com esse email
 
 
@@ -20,8 +27,27 @@ def cadastrar_usuario(
     comando: CriarUsuario, unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
 ) -> UsuarioId:
 
-    comando.senha = gerar_senha_encriptografada(comando.senha)
-    comando.senha_verifacao = gerar_senha_encriptografada(comando.senha_verifacao)
+    # verificar se usuario já existe
+    usuario = consultar_usuario_por_email(
+        BuscarUsuarioPorEmail(comando.email)
+    )
+    if usuario:
+        raise EmailJaCadastrado(
+            status_code=400,
+            detail='Email já cadastrado no sistema!'
+        )
+
+    # verificar se usuario digitou a mesma senha duas vezes
+    senha = Senha(comando.senha)
+    senha_verificacao = Senha(comando.senha_verifacao)
+    if senha != senha_verificacao:
+        raise SenhasDiferentes(
+            status_code=400,
+            detail='Senhas diferentes! Favor digite novamente.'
+        )
+    
+    comando.senha = gerar_senha_encriptografada(senha)
+    comando.senha_verifacao = gerar_senha_encriptografada(senha_verificacao)
 
     usuario = Usuario.criar_usuario(comando)
     usuario_id = usuario.id
