fb4ed110b4847279c2327afd0493c678e8cbc6e7
othonbreener <othonbrener123@gmail.com>
2023-01-25 22:22:20 -0300

    feat(garcom/config.py): adiciona envs para encpritar senha e criar token de acesso jwt

--- garcom/config.py
+++ garcom/config.py
@@ -7,6 +7,10 @@ class Configuracoes(BaseSettings):
     database_uri: str
     database_tests: str
     dsn_sentry: str
+    secret_key: str
+    algorithm: str
+    tipo_de_criptografia: str
+    tempo_de_expiracao: str
 
     class Config:
         """Classe que indica qual o arquivo .env."""
@@ -29,3 +33,19 @@ def get_postgres_tests() -> str:
 
 def get_dsn_sentry() -> str:
     return configs.dsn_sentry
+
+
+def get_secret_key() -> str:
+    return configs.secret_key
+
+
+def get_algorithm() -> str:
+    return configs.algorithm
+
+
+def get_tipo_de_criptografia() -> str:
+    return configs.tipo_de_criptografia
+
+
+def get_tempo_de_expiracao() -> int:
+    return int(configs.tempo_de_expiracao)
