08f6ecc7656f8b6193ab3008d7b21cf9bb9e8219
othonbreener <othonbrener123@gmail.com>
2023-01-26 20:45:00 -0300

    feat(garcom/contextos_de_negocio/identidade_e_acesso/excecoes.py): adiciona excecoes para senha diferentes e email já cadastrado no sistema

--- garcom/contextos_de_negocio/identidade_e_acesso/excecoes.py
+++ garcom/contextos_de_negocio/identidade_e_acesso/excecoes.py
@@ -6,3 +6,15 @@ from dataclasses import dataclass
 class SenhaIncorreta(HTTPException):
     status_code: int
     detail: str
+
+
+@dataclass
+class SenhasDiferentes(HTTPException):
+    status_code: int
+    detail: str
+
+
+@dataclass
+class EmailJaCadastrado(HTTPException):
+    status_code: int
+    detail: str
