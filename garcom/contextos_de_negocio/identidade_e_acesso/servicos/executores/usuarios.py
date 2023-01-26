830fc7e5e6cc8c4b4a70d68d9dcc81ddc39d68a5
othonbreener <othonbrener123@gmail.com>
2023-01-25 22:33:20 -0300

    feat(garcom/contextos_de_negocio/identidade_e_acesso/servicos): adiciona encriptacao de senha ao executor que cadastra usuario e implementa executores para buscar usuario por email e logar usuario no sistema

--- garcom/contextos_de_negocio/identidade_e_acesso/servicos/executores/usuarios.py
+++ garcom/contextos_de_negocio/identidade_e_acesso/servicos/executores/usuarios.py
@@ -9,13 +9,20 @@ from garcom.camada_de_servicos.unidade_de_trabalho.udt import (
     UnidadeDeTrabalhoAbstrata,
 )
 from garcom.adaptadores.dominio import Dominio
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.regras_de_negocio.encriptografia import (
+    gerar_senha_encriptografada, 
+)
 
+#TODO: fazer query por email para verificar se já existe usuario com esse email
 
 def cadastrar_usuario(
     comando: CriarUsuario, unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
 ) -> UsuarioId:
 
-    usuario = Usuario.cadrastar_novo_usuario(comando)
+    comando.senha = gerar_senha_encriptografada(comando.senha)
+    comando.senha_verifacao = gerar_senha_encriptografada(comando.senha_verifacao)
+    
+    usuario = Usuario.criar_usuario(comando)
     usuario_id = usuario.id
 
     with unidade_de_trabalho(Dominio.usuarios) as uow:
--- garcom/contextos_de_negocio/identidade_e_acesso/servicos/visualizadores/usuarios.py
+++ garcom/contextos_de_negocio/identidade_e_acesso/servicos/visualizadores/usuarios.py
@@ -8,11 +8,21 @@ from garcom.camada_de_servicos.unidade_de_trabalho.udt import (
 from garcom.contextos_de_negocio.identidade_e_acesso.dominio.comandos.usuario import (
     BuscarTodosUsuarios,
     BuscarUsuarioPorId,
+    BuscarUsuarioPorEmail,
+    LogarUsuario,
 )
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.regras_de_negocio.encriptografia import (
+    verificar_senha, criar_token_de_acesso
+)
+from garcom.contextos_de_negocio.identidade_e_acesso.excecoes import SenhaIncorreta
+from garcom.contextos_de_negocio.identidade_e_acesso.dominio.entidades.usuario import (
+    UsuarioLogado, UsuarioLeitura
+)
+from garcom.adaptadores.tipos_nao_primitivos.usuario import Email, Nome
 
 
 def consultar_usuarios(
-    comando: BuscarTodosUsuarios, unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
+    _: BuscarTodosUsuarios, unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
 ) -> list[Usuario]:
     with unidade_de_trabalho(Dominio.usuarios) as uow:
         usuarios = uow.repo_consulta.consultar_todos()
@@ -28,3 +38,56 @@ def consultar_usuario_por_id(
         usuario = uow.repo_consulta.consultar_por_id(comando.usuario_id)
 
     return usuario
+
+
+def consultar_usuario_por_email(
+    comando: BuscarUsuarioPorEmail,
+    unidade_de_trabalho: UnidadeDeTrabalhoAbstrata,
+) -> Usuario:
+    with unidade_de_trabalho(Dominio.usuarios) as uow:
+        usuario = uow.repo_consulta.consultar_por_email(comando.email)
+    
+    return usuario
+
+
+def login_de_usuario(
+    comando: LogarUsuario,
+    unidade_de_trabalho: UnidadeDeTrabalhoAbstrata
+) -> UsuarioLogado:
+
+    email = comando.email
+    senha = comando.senha
+    
+    comando_buscar_por_email = BuscarUsuarioPorEmail(
+        email=email
+    )
+    
+    usuario = consultar_usuario_por_email(
+        comando_buscar_por_email, unidade_de_trabalho
+    )
+    
+    validar_senha = verificar_senha(
+        senha=senha, senha_encriptada=usuario.senha
+    )
+    
+    if not validar_senha:
+        raise SenhaIncorreta(
+            status_code=400,
+            detail='Senha informada está incorreta, por favor digite a senha certa!'
+        )
+    
+    token_de_acesso = criar_token_de_acesso(
+        dado_de_acesso={'sub': usuario.email}
+    )
+    
+    usuario = UsuarioLeitura(
+        nome=Nome(usuario.nome),
+        email=Email(usuario.email),
+        ativo=usuario.ativo,
+        data_de_nascimento=usuario.data_de_nascimento,
+    )
+    
+    return UsuarioLogado(
+        usuario=usuario,
+        token_de_acesso=token_de_acesso,
+    )
