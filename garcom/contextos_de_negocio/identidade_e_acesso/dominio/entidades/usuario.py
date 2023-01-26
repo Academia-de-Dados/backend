ce9806143b60a27209e48b650a62f9bc424b45b5
othonbreener <othonbrener123@gmail.com>
2023-01-25 22:27:40 -0300

    feat(garcom/contextos_de_negocio/identidade_e_acesso/dominio/entidades/usuario.py): adiciona classe para excessao http  para senha incorreta

---
+++ garcom/contextos_de_negocio/identidade_e_acesso/dominio/entidades/usuario.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from datetime import datetime
+from garcom.adaptadores.tipos_nao_primitivos.usuario import Email, Nome
+from garcom.adaptadores.tipos_nao_primitivos.tipos import UsuarioId
+from dataclass_type_validator import dataclass_validate
+
+
+@dataclass_validate
+@dataclass(frozen=True)
+class UsuarioLeitura:
+    nome: Nome
+    email: Email
+    ativo: bool
+    data_de_nascimento: datetime
+
+
+@dataclass_validate
+@dataclass(frozen=True)
+class UsuarioLogado:
+    usuario: UsuarioLeitura
+    token_de_acesso: str
