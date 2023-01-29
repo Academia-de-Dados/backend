75529c0397665a3bf658c0daac722f86dcd749f2
othonbreener <othonbrener123@gmail.com>
2023-01-29 20:56:54 -0300

    feat(garcom/contextos_de_negocio/barramento/identidade_e_acesso.py): adiciona comando BuscarUsuarioPorEmail nos manipuladores do contexto de identidade e acesso

--- garcom/contextos_de_negocio/barramento/identidade_e_acesso.py
+++ garcom/contextos_de_negocio/barramento/identidade_e_acesso.py
@@ -2,6 +2,7 @@ from garcom.contextos_de_negocio.identidade_e_acesso.servicos.visualizadores.usu
     consultar_usuarios,
     consultar_usuario_por_id,
     login_de_usuario,
+    consultar_usuario_por_email
 )
 from garcom.contextos_de_negocio.identidade_e_acesso.servicos.executores.usuarios import (
     cadastrar_usuario,
@@ -11,6 +12,7 @@ from garcom.contextos_de_negocio.identidade_e_acesso.dominio.comandos.usuario im
     BuscarTodosUsuarios,
     BuscarUsuarioPorId,
     LogarUsuario,
+    BuscarUsuarioPorEmail,
 )
 
 
@@ -19,6 +21,7 @@ MANIPULADORES_IDENTIDADE_E_ACESSO_EVENTOS = {}
 MANIPULADORES_IDENTIDADE_E_ACESSO_COMANDOS = {
     BuscarTodosUsuarios: consultar_usuarios,
     BuscarUsuarioPorId: consultar_usuario_por_id,
+    BuscarUsuarioPorEmail: consultar_usuario_por_email,
     CriarUsuario: cadastrar_usuario,
     LogarUsuario: login_de_usuario,
 }
