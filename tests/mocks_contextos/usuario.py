026353107bf1393983730dee4f30cc1fe99c539d
othonbreener <othonbrener123@gmail.com>
2023-03-19 22:16:03 -0300

    style: black .

--- tests/mocks_contextos/usuario.py
+++ tests/mocks_contextos/usuario.py
@@ -1,55 +1,51 @@
 from pytest import fixture
 
 
-@fixture(scope='function')
+@fixture(scope="function")
 def mock_usuario_gen(cliente, session):
-    
     def criar_mock(
-        nome: str = 'Usuario Teste',
-        email: str = 'usuarioteste@gmail.com',
-        senha: str = 'SenhaTeste123',
-        senha_verificacao: str = 'SenhaTeste123',
-        data_de_nascimento: str = "1998-03-12T00:00:00.901Z"
+        nome: str = "Usuario Teste",
+        email: str = "usuarioteste@gmail.com",
+        senha: str = "SenhaTeste123",
+        senha_verificacao: str = "SenhaTeste123",
+        data_de_nascimento: str = "1998-03-12T00:00:00.901Z",
     ):
         usuario = {
-            'nome': nome,
-            'email': email,
-            'senha': senha,
-            'senha_verificacao': senha_verificacao,
-            'data_de_nascimento': data_de_nascimento,
+            "nome": nome,
+            "email": email,
+            "senha": senha,
+            "senha_verificacao": senha_verificacao,
+            "data_de_nascimento": data_de_nascimento,
         }
-        
-        resposta = cliente.post('/usuarios/signup', json=usuario)
-        
+
+        resposta = cliente.post("/usuarios/signup", json=usuario)
+
         assert resposta.status_code == 200
-        
+
         return resposta.json()
-    
+
     yield criar_mock
 
 
-@fixture(scope='function')
+@fixture(scope="function")
 def mock_gerar_token_autenticado(mock_usuario_gen, cliente, session):
-    
     def criar_mock():
         id_usuario = mock_usuario_gen()
-        
+
         dados = {
-            'username': 'usuarioteste@gmail.com',
-            'password': 'SenhaTeste123',
-            'grant_type': 'password',
+            "username": "usuarioteste@gmail.com",
+            "password": "SenhaTeste123",
+            "grant_type": "password",
         }
-        
+
         resposta = cliente.post(
-            '/usuarios/signin', 
+            "/usuarios/signin",
             data=dados,
-            headers={
-                "content-type": "application/x-www-form-urlencoded"
-            }
+            headers={"content-type": "application/x-www-form-urlencoded"},
         )
 
         assert resposta.status_code == 200
-        
-        return id_usuario, resposta.json()['access_token']
-    
+
+        return id_usuario, resposta.json()["access_token"]
+
     yield criar_mock
--- tests/testes_funcionais/identidade_e_acesso/test_pontos_de_entrada_api.py
+++ tests/testes_funcionais/identidade_e_acesso/test_pontos_de_entrada_api.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import sessionmaker
 from garcom.contextos_de_negocio.identidade_e_acesso.dominio.regras_de_negocio.encriptografia import (
-    validar_token_de_acesso
+    validar_token_de_acesso,
 )
 
 
@@ -18,14 +18,14 @@ def test_cadastrar_usuario(cliente: TestClient, session: sessionmaker):
         },
     )
 
-    usuario_id = session.execute('SELECT * FROM usuarios').scalar()
-    
+    usuario_id = session.execute("SELECT * FROM usuarios").scalar()
+
     assert resposta.status_code == 200
     assert resposta.json() == str(usuario_id)
 
 
 def test_logar_usuario(cliente: TestClient, session: sessionmaker):
-    
+
     cadastro = cliente.post(
         "/usuarios/signup",
         json={
@@ -36,53 +36,46 @@ def test_logar_usuario(cliente: TestClient, session: sessionmaker):
             "data_de_nascimento": "1998-03-12T00:00:00.901Z",
         },
     )
-    
+
     assert cadastro.status_code == 200
-    
+
     logar = cliente.post(
         "/usuarios/signin",
         data={
-            'username': 'cairon@gmail.com',
-            'password': 'Cairon123',
-            'grant_type': 'password',
+            "username": "cairon@gmail.com",
+            "password": "Cairon123",
+            "grant_type": "password",
         },
-        headers={
-            "content-type": "application/x-www-form-urlencoded"
-        }
+        headers={"content-type": "application/x-www-form-urlencoded"},
     )
 
     assert logar.status_code == 200
-    assert logar.json()['usuario']['nome'] == "Cairon Henrique"
-    assert logar.json()['usuario']['email'] == "cairon@gmail.com"
-    assert logar.json()['usuario']['ativo'] == True
-    
-    token =  logar.json()['access_token']
+    assert logar.json()["usuario"]["nome"] == "Cairon Henrique"
+    assert logar.json()["usuario"]["email"] == "cairon@gmail.com"
+    assert logar.json()["usuario"]["ativo"] == True
+
+    token = logar.json()["access_token"]
     validar_token = validar_token_de_acesso(token)
-    
-    assert validar_token.sub == 'cairon@gmail.com'
+
+    assert validar_token.sub == "cairon@gmail.com"
 
 
 def test_consultar_usuarios(
-    cliente: TestClient, 
+    cliente: TestClient,
     session: sessionmaker,
     mock_gerar_token_autenticado,
 ):
     _, token = mock_gerar_token_autenticado()
-    
-    usuarios = cliente.get(
-        '/usuarios/',
-        headers={
-            'Authorization': 'Bearer ' + token
-        }
-    )
-    
+
+    usuarios = cliente.get("/usuarios/", headers={"Authorization": "Bearer " + token})
+
     assert usuarios.status_code == 200
     assert isinstance(usuarios.json(), list)
     assert len(usuarios.json()) == 1
-    assert usuarios.json()[0]['nome'] == 'Usuario Teste'
-    assert usuarios.json()[0]['email'] == 'usuarioteste@gmail.com'
-    
-    
+    assert usuarios.json()[0]["nome"] == "Usuario Teste"
+    assert usuarios.json()[0]["email"] == "usuarioteste@gmail.com"
+
+
 def test_consultar_usuario_por_id(
     cliente: TestClient,
     session: sessionmaker,
@@ -91,12 +84,9 @@ def test_consultar_usuario_por_id(
     id_usuario, token = mock_gerar_token_autenticado()
 
     usuarios = cliente.get(
-        f'/usuarios/{id_usuario}',
-        headers={
-            'Authorization': 'Bearer ' + token
-        }
+        f"/usuarios/{id_usuario}", headers={"Authorization": "Bearer " + token}
     )
 
     assert usuarios.status_code == 200
-    assert usuarios.json()['nome'] == 'Usuario Teste'
-    assert usuarios.json()['email'] == 'usuarioteste@gmail.com'
+    assert usuarios.json()["nome"] == "Usuario Teste"
+    assert usuarios.json()["email"] == "usuarioteste@gmail.com"
