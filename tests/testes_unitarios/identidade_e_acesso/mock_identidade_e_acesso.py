7e3d98bacbeb32f3e8aa6ff62bd1cdc69c3716cd
othonbreener <othonbrener123@gmail.com>
2023-04-02 14:42:51 -0300

    fix: arrumando mock de inserir usuario

--- tests/testes_unitarios/identidade_e_acesso/mock_identidade_e_acesso.py
+++ tests/testes_unitarios/identidade_e_acesso/mock_identidade_e_acesso.py
@@ -10,8 +10,8 @@ def inserir_usuario(session: sessionmaker, comando: CriarUsuario):
 
     usuario_id = UsuarioId()
     session.execute(
-        "INSERT INTO usuarios (id, nome, senha, email, data_de_nascimento, ativo, criado_em, ultima_modificacao)"
-        "VALUES (:id, :nome, :senha, :email, :data_de_nascimento, :ativo, :criado_em, :ultima_modificacao)",
+        "INSERT INTO usuarios (id, nome, senha, email, data_de_nascimento, ativo, tipo_de_acesso, criado_em, ultima_modificacao)"
+        "VALUES (:id, :nome, :senha, :email, :data_de_nascimento, :ativo, :tipo_de_acesso, :criado_em, :ultima_modificacao)",
         dict(
             id=usuario_id,
             nome=comando.nome,
@@ -19,6 +19,7 @@ def inserir_usuario(session: sessionmaker, comando: CriarUsuario):
             email=comando.email,
             data_de_nascimento=comando.data_de_nascimento,
             ativo=True,
+            tipo_de_acesso=comando.tipo_de_acesso.value,
             criado_em=datetime.now(),
             ultima_modificacao=datetime.now(),
         ),
