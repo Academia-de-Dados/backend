f01afeb6745cc23e5f0398c0a0d7b2c1b8b99aec
othonbreener <othonbrener123@gmail.com>
2023-04-06 19:34:41 -0300

    fix(garcom/aplicacao/sentry.py): remove cometario do sentry e habilita ele de novo

--- garcom/aplicacao/sentry.py
+++ garcom/aplicacao/sentry.py
@@ -8,14 +8,13 @@ from garcom.config import get_dsn_sentry
 
 loggers = logger
 
-# if get_dsn_sentry():
-#    init(
-#        dsn=get_dsn_sentry(),
-#        integrations=[LoggingIntegration(level=None, event_level=None)],
-#        # Set traces_sample_rate to 1.0 to capture 100%
-#        # of transactions for performance monitoring.
-#        # We recommend adjusting this value in production.
-#        traces_sample_rate=1.0,
-#    )
-#
-#    loggers.add(EventHandler(level=ERROR), format='{time} {level} {message}')
+if get_dsn_sentry():
+   init(
+       dsn=get_dsn_sentry(),
+       integrations=[LoggingIntegration(level=None, event_level=None)],
+       # Set traces_sample_rate to 1.0 to capture 100%
+       # of transactions for performance monitoring.
+       # We recommend adjusting this value in production.
+       traces_sample_rate=1.0,
+   )
+   loggers.add(EventHandler(level=ERROR), format='{time} {level} {message}')
